582dd9a7fe809d92ef72efa6f8b7d62ba25fbd3f
Crystal <crystal@easychat.co>
2020-02-03 11:06:27 +0800

    modify initial time display

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -10,17 +10,13 @@ function setDate() {
     secondHand.style.transform = `rotate(${secondsDegree}deg)`
     
     const minutes = now.getMinutes();
-    const minutesDegree = ((minutes / 60) * 369) + 90
+    const minutesDegree = ((minutes / 60) * 360) + 90
     minuteHand.style.transform = `rotate(${minutesDegree}deg)`
 
     const hours = now.getHours();
-    const hoursDegree = ((hours / 12) * 369) + 90
+    const hoursDegree = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90
     hourHand.style.transform = `rotate(${hoursDegree}deg)`
-
-    
 }
-
-setInterval(() => {
-    setDate()
-}, 1000);
+setDate();
+setInterval(setDate,1000);
 
